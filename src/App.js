32f68e03ff6db0471e10114892ac437d7fc8ebe5
Parker Mathis <parkerjmathis@gmail.com>
2023-03-28 18:28:11 -0700

    Added placeholder navbar

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
 import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar/index";
 
 function App() {
-  return <Router></Router>;
+  return (
+    <Router>
+      <Navbar />
+      <Routes></Routes>
+    </Router>
+  );
 }
 
 export default App;
