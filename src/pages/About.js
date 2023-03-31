c067d9ad34b23b4224fe87e7f80972eeda352a70
Parker Mathis <parkerjmathis@gmail.com>
2023-03-30 18:43:19 -0700

    Added placeholder about page content

--- src/pages/About.js
+++ src/pages/About.js
@@ -2,8 +2,16 @@ import React from "react";
 
 export default function About() {
   return (
-    <div className="content">
-      <h2>About Me</h2>
+    <div className="flex flex-col items-center pt-16 px-64">
+      <h2 className="text-3xl font-bold">About Me</h2>
+      <p>
+        <br />
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed tellus quam, faucibus id dolor eu, posuere imperdiet nunc. Ut hendrerit ligula eget porta sodales. Vestibulum ante ipsum primis in
+        faucibus orci luctus et ultrices posuere cubilia curae; Nulla at tortor a lacus fringilla ultricies eu et libero. Vestibulum a mauris dui. Sed convallis massa venenatis massa dapibus, eu
+        interdum nibh congue. Quisque hendrerit sed felis nec porttitor. Duis quis arcu vel tellus viverra blandit eu nec urna. Nulla facilisi. Curabitur arcu quam, finibus a vehicula et, porttitor et
+        dui. Donec metus diam, euismod eu purus consequat, porta placerat purus. Cras vestibulum orci at tellus pharetra euismod. Interdum et malesuada fames ac ante ipsum primis in faucibus. Nam sit
+        amet placerat eros. Cras aliquam elit lectus, vel semper lacus tincidunt a. Ut viverra, nulla nec vestibulum dapibus, odio nisl pharetra est, ut commodo massa quam id metus.
+      </p>
     </div>
   );
 }
