149d797c221864e17d1a6795c62a7c47406bff91
Parker Mathis <parkerjmathis@gmail.com>
2023-03-30 17:16:30 -0700

    Update footer with tailwind classes

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -2,28 +2,16 @@ import React from "react";
 import logoGitHub from "../../assets/logoGitHub.png";
 import logoLinkedIn from "../../assets/logoLinkedIn.png";
 
-const styles = {
-  footer: {
-    display: "flex",
-    justifyContent: "center",
-    alignItems: "center",
-    paddingTop: "50px",
-  },
-  icons: {
-    paddingLeft: "15px",
-  },
-};
-
 export default function Footer() {
   return (
-    <div style={styles.footer}>
-      <div>
-        <a href="https://github.com/markerpathis" target="_blank" rel="noreferrer" style={styles.icons}>
+    <div className="pt-14 flex justify-center ">
+      <div className="px-2">
+        <a href="https://github.com/markerpathis" target="_blank" rel="noreferrer">
           <img src={logoGitHub} alt="GitHub Logo" height="50px" />
         </a>
       </div>
-      <div>
-        <a href="https://www.linkedin.com/in/parker-mathis/" target="_blank" rel="noreferrer" style={styles.icons}>
+      <div className="px-2">
+        <a href="https://www.linkedin.com/in/parker-mathis/" target="_blank" rel="noreferrer">
           <img src={logoLinkedIn} alt="LinkedIn Logo" height="50px" />
         </a>
       </div>
