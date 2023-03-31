5d5d08ed93b9fd704735e9622ac76ad24628fd4e
Parker Mathis <parkerjmathis@gmail.com>
2023-03-30 18:22:12 -0700

    Added starter home page content and photo

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -12,7 +12,7 @@ export default function Navbar() {
               class="block py-2 pl-3 pr-4 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 md:dark:text-white dark:bg-blue-600 md:dark:bg-transparent"
               aria-current="page"
             >
-              Home
+              Parker Mathis
             </a>
           </li>
 
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,12 @@
 import React from "react";
+import parkerImage from "../assets/parkerImage.png";
 
 export default function Home() {
   return (
-    <div className="content">
-      <h2>Home</h2>
+    <div className="flex flex-col items-center pt-16">
+      <img src={parkerImage} className="h-48" />
+      <h2 className="text-3xl font-bold pt-8">Hey there! I'm Parker.</h2>
+      <h2 className="text-2xl pt-8">I'm a recent coding bootcamp grad, seeking a career as a full-stack developer.</h2>
     </div>
   );
 }
