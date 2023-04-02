917f1676959b5d1d85de809a1d6013cdaaf47057
Parker Mathis <parkerjmathis@gmail.com>
2023-04-02 15:36:30 -0700

    Added details to contact page

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -2,8 +2,15 @@ import React from "react";
 
 export default function Contact() {
   return (
-    <div className="content">
-      <h2>Contact</h2>
+    <div className="flex flex-col items-center pt-16 px-64 mb-24">
+      <h2 className="text-3xl font-bold">Contact</h2>
+      <p>
+        <br></br>Get in touch! My contact details are included below: <br></br>
+        <br></br>Parker Mathis
+        <br></br>425-418-2121
+        <br></br>
+        <a href="mailto:parkerjmathis@gmail.com?subject=Developer Portfolio Contact Request">parkerjmathis@gmail.com</a>
+      </p>
     </div>
   );
 }
