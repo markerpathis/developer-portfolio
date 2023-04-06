c799940e718163a46d411678889bd334bcff2db2
Parker Mathis <parkerjmathis@gmail.com>
2023-04-05 18:10:39 -0700

    Update portfolio card responsiveness

--- src/components/PortfolioCard/index.js
+++ src/components/PortfolioCard/index.js
@@ -2,10 +2,10 @@ import React from "react";
 
 function PortfolioCard(props) {
   return (
-    <div className="mx-auto max-w-2xl sm:py-6 lg:max-w-5xl lg:px-8">
-      <div className="grid grid-cols-1 gap-y-10 gap-x-6 md:grid-cols-1 lg:grid-cols-2">
+    <div className="mx-auto max-w-2xl sm:py-6 lg:max-w-max lg:px-8">
+      <div className="grid gap-y-10 gap-x-6 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
         {props.portfolioContent.map((item) => (
-          <div className="max-w-xl rounded overflow-hidden border-2 border-white hover:border-[#49AB8C] bg-[#303634] hover:bg-[#434C49] md:max-w-lg" key={item.id}>
+          <div className="max-w-sm rounded overflow-hidden border-2 border-white hover:border-[#49AB8C] bg-[#303634] hover:bg-[#434C49]" key={item.id}>
             <img className="w-full" src={item.image} alt=""></img>
             <div className="px-6 py-4 pb-2">
               <div className="font-bold text-xl mb-2">{item.title}</div>
--- src/pages/Portfolio.js
+++ src/pages/Portfolio.js
@@ -5,7 +5,8 @@ import portfolioContent from "../utils/portfolioContent";
 export default function Portfolio() {
   return (
     <>
-      <div className="flex flex-col items-center pt-16 px-64 mb-24">
+      {/* <div className="flex flex-col items-center pt-16 px-64 mb-24"> */}
+      <div className="flex flex-col items-center pt-16 mb-24">
         <h2 className="text-3xl font-bold">Portfolio</h2>
         <PortfolioCard portfolioContent={portfolioContent} />
       </div>
